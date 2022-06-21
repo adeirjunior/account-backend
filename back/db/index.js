6ad10f844c210dbf390f884b74fcc7ac02af020a
Adeir do Bom Fim Junior <4adeirj@gmail.com>
2022-06-21 14:02:44 -0400

    vercel settings

--- back/db/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const runDatabase = (uri) => {
-    (0, mongoose_1.connect)(uri);
-};
-exports.default = runDatabase;
