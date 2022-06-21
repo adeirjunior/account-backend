c5f4cc8245bc8cbadeb043988dd82c4eac4df793
Adeir do Bom Fim Junior <4adeirj@gmail.com>
2022-06-21 00:34:03 -0400

    test

--- back/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userApi = void 0;
-var user_1 = require("./user");
-Object.defineProperty(exports, "userApi", { enumerable: true, get: function () { return __importDefault(user_1).default; } });
